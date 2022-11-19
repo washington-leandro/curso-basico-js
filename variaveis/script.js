13a4ea66b93b7ed625261d5d7477295f03afb431
Washington Leandro <sntslleandro@gmail.com>
2022-11-19 15:07:14 -0300

    first commit

---
+++ variaveis/script.js
@@ -0,0 +1,35 @@
+/*Variaveis sao responsaveis por guardar
+* dados na memoria.
+* Inicia com a palavra var, let ou const 
+DRY (Dont repeat yourself) */
+
+/* var nome = "Washington";
+let idade = 29;
+var possuiFaculdade = true; */
+
+/* var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco; */
+
+//EXERCICIOS
+
+// Declarar uma variavel com o seu nome
+var nome = "Washington";
+console.log(nome);
+
+// Declarar uma variavel com a sua idade
+var idade = 29;
+console.log(idade);
+
+/* Declarar uma variavel com a  sua comida
+favorita e nao atribuir valor*/
+var comidaFavorita;
+console.log(comidaFavorita);
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Feijao com arroz e banana frita"
+console.log(comidaFavorita);
+
+// Declarar 5 variaveis diferentes sem valores
+var time, professor, aluno, rua, ano;
+console.log();

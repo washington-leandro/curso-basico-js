a57b9135cf967b8cf7044badf45d3f95028146af
Washington Leandro <sntslleandro@gmail.com>
2022-11-19 16:24:53 -0300

    Inserção tipo de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,28 @@
+/*
+var nome = "Washington"; //String
+var idade = 29; // Number
+var time = null;
+var simbolo = Symbol(); // Symbol 
+*/
+
+/*
+var nome = "Washington ";
+var sobrenome = "Leandro";
+var nomeCompleto = nome + sobrenome;
+*/
+
+/*
+var gols = 1000;
+var frase = 'Pelé fez ' + gols + ' gols.'
+*/
+
+/*
+var melhor = 'teste';
+var frase1 = "Esse é o \"melhor\" jogo";
+*/
+
+// TEMPLATE STRING
+var gols = 1000;
+var frase2 = `Pelé fez ${gols*3} gols.`; //Usando template String
+
+console.log(frase2);
